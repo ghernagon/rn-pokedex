b5303477315912c0c1f8e1f0b39f7f81ededaa8d
Gerardo Hernandez <hernandez.gerardo@bcgplatinion.com>
2023-04-20 09:52:17 -0400

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigator} from './src/navigation/ListTab';
 import {Tabs} from './src/navigation/Tabs';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       {/* <Navigator /> */}
--- android/app/src/main/java/com/pokedex/MainActivity.java
+++ android/app/src/main/java/com/pokedex/MainActivity.java
@@ -1,5 +1,7 @@
 package com.pokedex;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -10,6 +12,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
